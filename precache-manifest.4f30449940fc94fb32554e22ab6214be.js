5e0532df0ecc58fc43c0962caa7bfa5c186244b9
Silvia <silvia.alexa.silva@gmail.com>
2020-03-21 16:20:27 +0000

    Updates

---
+++ precache-manifest.4f30449940fc94fb32554e22ab6214be.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df8de4d9a50577637fe3246dde539518",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2214b7af02208ebdc077",
+    "url": "/static/css/2.df9ef6f4.chunk.css"
+  },
+  {
+    "revision": "dd7c9637ccfc0d683ec4",
+    "url": "/static/css/main.75591b53.chunk.css"
+  },
+  {
+    "revision": "2214b7af02208ebdc077",
+    "url": "/static/js/2.0aeb3122.chunk.js"
+  },
+  {
+    "revision": "dd7c9637ccfc0d683ec4",
+    "url": "/static/js/main.23945ee2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3af178ecd64adfa098e7456e5830e5f8",
+    "url": "/static/media/Game of Thrones.3af178ec.ttf"
+  },
+  {
+    "revision": "c7ad8f9ccacde5ba6568b999b1a4c3ad",
+    "url": "/static/media/House-Arryn.c7ad8f9c.png"
+  },
+  {
+    "revision": "33ab9b28fdfe8354373e00659b200762",
+    "url": "/static/media/House-Baratheon.33ab9b28.png"
+  },
+  {
+    "revision": "f42736c3cfda1c8b1a39b4c3ad740229",
+    "url": "/static/media/House-Bolton.f42736c3.png"
+  },
+  {
+    "revision": "3895cb1f99b64b8498f070a39bfe3cf0",
+    "url": "/static/media/House-Frey.3895cb1f.png"
+  },
+  {
+    "revision": "760d3ae0aed500e708948558c6379abe",
+    "url": "/static/media/House-Greyjoy.760d3ae0.png"
+  },
+  {
+    "revision": "8b2f12e64b55f4d547efb7e1cf2556fb",
+    "url": "/static/media/House-Lannister.8b2f12e6.png"
+  },
+  {
+    "revision": "5c66d2317e6461becd2776bd39f2d721",
+    "url": "/static/media/House-Martell.5c66d231.png"
+  },
+  {
+    "revision": "12922304a9727fca1e9fc5c766aae55f",
+    "url": "/static/media/House-Stark.12922304.png"
+  },
+  {
+    "revision": "7f9b9addf9b5e5e54421dbd26742ac76",
+    "url": "/static/media/House-Targaryen.7f9b9add.png"
+  },
+  {
+    "revision": "a7b3a33811e90443d1a2df7b74e7afb3",
+    "url": "/static/media/House-Tarly.a7b3a338.png"
+  },
+  {
+    "revision": "1d404dee0d7a0655d6c5aefba4f36cdd",
+    "url": "/static/media/House-Tully.1d404dee.png"
+  },
+  {
+    "revision": "48bf5991e23df884254d3cf9f37ae7d8",
+    "url": "/static/media/House-Tyrell.48bf5991.png"
+  },
+  {
+    "revision": "c9b7ee2c314d78450a727a336258ce9a",
+    "url": "/static/media/book1.c9b7ee2c.jpg"
+  },
+  {
+    "revision": "0cb568328e225a9570c3d40e44f13e98",
+    "url": "/static/media/house.0cb56832.jpg"
+  },
+  {
+    "revision": "f3e0c076e57b5f9ef7b046575af1d80b",
+    "url": "/static/media/original.f3e0c076.png"
+  },
+  {
+    "revision": "3dc63ca37c26b07ef257857f97141c0c",
+    "url": "/static/media/wallpaper.3dc63ca3.jpg"
+  },
+  {
+    "revision": "a08928f3570ffe5e9afeef580e1b31cb",
+    "url": "/static/media/wallpaper3.a08928f3.jpg"
+  },
+  {
+    "revision": "15fbc34bfdaed04520eb8a7372a0fc0e",
+    "url": "/static/media/wallpaper4.15fbc34b.jpg"
+  },
+  {
+    "revision": "1123cbad93a706eee50bf12524152fc7",
+    "url": "/static/media/westeros_map.1123cbad.jpg"
+  }
+]);
